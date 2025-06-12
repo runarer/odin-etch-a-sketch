1c3ff2de77d9defa77c7c1494dbf6c5fffbb0093
runarer <95697611+runarer@users.noreply.github.com>
2025-06-12 18:02:22 +0200

    Adds user input functionality

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ function colorBackground(event) {
     this.style.background = "blue";
 }
 
+function askUserForSize() {
+    const sizeFromUser = prompt("Please enter a size of the grid, between 1 and 100");
+    const size = parseInt(sizeFromUser);
+    if (size !== NaN && size !== null && size > 0 && size <= 100) {
+        // const etchGrid  = document.querySelector(".etch-row");
+        createGrid(size);        
+    }
+}
+
 function createGrid(size) {
     const etchGrid = document.querySelector(".etch-grid");
 
+    etchGrid.replaceChildren();
+    
     for (let rowNr = 0; rowNr < size; rowNr++) {
         const etchRow = document.createElement("div")
         etchRow.classList.add("etch-row");
@@ -22,3 +33,6 @@ function createGrid(size) {
 }
 
 createGrid(16);
+
+const sizeSelection = document.querySelector("#size-selection");
+sizeSelection.addEventListener("click",askUserForSize);
