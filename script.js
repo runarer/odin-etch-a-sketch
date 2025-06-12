308d8cfce00a8e0ce8cb3b5542b7e438723d14b3
runarer <95697611+runarer@users.noreply.github.com>
2025-06-12 18:32:47 +0200

    Adds progressive darkening

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function colorBackground(event) {
     this.style.background = "blue";
+
+    const currentOpacity = parseFloat(window.getComputedStyle(this).opacity);
+    this.style.opacity = currentOpacity + 0.1
 }
 
 function askUserForSize() {
     const sizeFromUser = prompt("Please enter a size of the grid, between 1 and 100");
     const size = parseInt(sizeFromUser);
     if (size !== NaN && size !== null && size > 0 && size <= 100) {
-        // const etchGrid  = document.querySelector(".etch-row");
         createGrid(size);        
     }
 }
@@ -14,15 +16,16 @@ function askUserForSize() {
 function createGrid(size) {
     const etchGrid = document.querySelector(".etch-grid");
 
+    // Remove children if there are any, resets the grid.
     etchGrid.replaceChildren();
-    
+
     for (let rowNr = 0; rowNr < size; rowNr++) {
         const etchRow = document.createElement("div")
         etchRow.classList.add("etch-row");
         
         for(let columnNr = 0; columnNr < size; columnNr++) {
             const etchSquare = document.createElement("div");
-            etchSquare.classList.add("etch-square");
+            etchSquare.classList.add("etch-square");            
             
             etchSquare.addEventListener("mouseenter",colorBackground);
 
